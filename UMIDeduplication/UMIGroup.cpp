ce0280b0a9dee702c3c12c43ac227a1521b02a3c
gpeng <michael.gang.peng@gmail.com>
2021-03-29 11:17:03 -0400

    add UMIGroup

---
+++ UMIDeduplication/UMIGroup.cpp
@@ -0,0 +1,9 @@
+//
+//  UMIGroup.cpp
+//  UMIDeduplication
+//
+//  Created by Gang Peng on 3/29/21.
+//  Copyright © 2021 Gang Peng. All rights reserved.
+//
+
+#include "UMIGroup.hpp"
---
+++ UMIDeduplication/UMIGroup.hpp
@@ -0,0 +1,18 @@
+//
+//  UMIGroup.hpp
+//  UMIDeduplication
+//
+//  Created by Gang Peng on 3/29/21.
+//  Copyright © 2021 Gang Peng. All rights reserved.
+//
+
+#ifndef UMIGroup_hpp
+#define UMIGroup_hpp
+
+#include <stdio.h>
+
+class UMIGroup{
+    
+};
+
+#endif /* UMIGroup_hpp */
