fc6792f03ff15ee54dbde87846da2f9d6cb8a466
gpeng <michael.gang.peng@gmail.com>
2021-03-30 14:17:36 -0400

    add UMISingleGroup for single read fastq file

---
+++ UMIDeduplication/UMISingleGroup.cpp
@@ -0,0 +1,9 @@
+//
+//  UMISingleGroup.cpp
+//  UMIDeduplication
+//
+//  Created by Gang Peng on 3/30/21.
+//  Copyright © 2021 Gang Peng. All rights reserved.
+//
+
+#include "UMISingleGroup.hpp"
---
+++ UMIDeduplication/UMISingleGroup.hpp
@@ -0,0 +1,14 @@
+//
+//  UMISingleGroup.hpp
+//  UMIDeduplication
+//
+//  Created by Gang Peng on 3/30/21.
+//  Copyright © 2021 Gang Peng. All rights reserved.
+//
+
+#ifndef UMISingleGroup_hpp
+#define UMISingleGroup_hpp
+
+#include <stdio.h>
+
+#endif /* UMISingleGroup_hpp */
